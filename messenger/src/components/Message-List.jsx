3b4f78cf84da6191ee2f8c7ad3f15546f0acc790
Cameron Thornton <shubwub@gmail.com>
2019-11-13 11:17:00 +0000

    adds react components and styling setup

---
+++ messenger/src/components/Message-List.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import '../styles/Message-List.css'
+
+
+export default class MessageList extends Component {
+  render() {
+    return (
+      <ul className="messages">
+        message-list
+      </ul>
+    )
+  }
+}
