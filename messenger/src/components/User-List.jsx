5b064b79862b7593384adf86ac934cac2c9530f9
By-Tor2114 <by_tor2114_uk@yahoo.com>
2019-11-13 14:52:37 +0000

    adds a list of users in User-List

--- messenger/src/components/User-List.jsx
+++ messenger/src/components/User-List.jsx
@@ -1,12 +1,31 @@
-import React, { Component } from 'react'
-import '../styles/User-List.css'
+import React, { Component } from 'react';
+import '../styles/User-List.css';
+import * as querying from '../utils/querying';
 
 export default class UserList extends Component {
+  state = {
+    users: [],
+    isLoading: true
+  };
+
+  componentDidMount = async () => {
+    const users = await querying.getUsers();
+    this.setState({ users, isLoading: false });
+  };
+
   render() {
+    const { users, isLoading } = this.state;
+
     return (
-      <ul className="users">
-        user-list
+      <ul className="UserList">
+        {isLoading ? (
+          <h1>Usernames Loading...</h1>
+        ) : (
+          users.map(user => {
+            return <li key={user.user_id}>{user.username}</li>;
+          })
+        )}
       </ul>
-    )
+    );
   }
 }
--- messenger/src/utils/querying.js
+++ messenger/src/utils/querying.js
@@ -1,8 +1,14 @@
 const axios = require('axios');
+const baseURL = 'http://localhost:9090/api';
 
 const getMessages = async () => {
   const { data } = await axios.get('http://localhost:9090/api/messages');
   return data.messages;
 };
 
-export { getMessages };
+const getUsers = async () => {
+  const { data } = await axios.get(`${baseURL}/users`);
+  return data.users;
+};
+
+export { getMessages, getUsers };
